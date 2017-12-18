ff3b199b18667a86bf1c1adb68c7f82c7b193cfe
Omar <omarmohmaha@yahoo.com>
2017-12-18 19:46:03 +0400

    Advent of Code Day 13 Solution

--- Day-13/layer.js
+++ Day-13/layer.js
@@ -2,24 +2,9 @@ class Layer {
   constructor(depth, range) {
     this.depth = depth;
     this.range = range-1;
-    if (range > 0) {
-      this.scanner = 0;
-      this.dir = 1;
-    }
   }
 
   isAtTop(time) {
     return (time % (this.range * 2)) === 0;
-  }
-
-  static advance(arr) {
-    for (let layer of arr) {
-      if (layer.range > 0) {
-        layer.scanner += layer.dir;
-        if (layer.scanner === layer.range || layer.scanner === 0) {
-          layer.dir *= -1;
-        }
-      }
-    }
   }
 }
